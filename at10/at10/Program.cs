abea12ff975d5c845627f7b28695bdce33bbf634
Robert <132854942+Robertinhou@users.noreply.github.com>
2025-02-03 08:15:24 -0300

    Add files via upload

---
+++ at10/at10/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace at10
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Digite um número de ponto flutuante");
+			float num = float.Parse(Console.ReadLine());
+			
+			Console.WriteLine((num == 0)? "é igual a 0" : "diferente de 0" );
+			
+			Console.Write("Pressione algo para sair . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
